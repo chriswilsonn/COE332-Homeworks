14e82d5d9f64b209afe13a593213a0150a8b7e82
Ubuntu <ubuntu@coe332-vm-5.js2local>
2025-01-29 16:45:48 +0000

    created a function to classify which hemisphere each meteorite landing took place in and grouped them together based on location

--- homework01/ml_json_reader.py
+++ homework01/ml_json_reader.py
@@ -16,6 +16,19 @@ def meteor_classifier(list_of_dicts, key_string):
 
     return(word_count_dict)
 
+def check_hemisphere(latitude: float, longitude: float) -> str:
+    location = []
+    if (latitude > 0):
+        location.append('Northern')
+    else:
+        location.append('Southern')
+    if (longitude > 0):
+        location.append(f'{location} & Eastern')
+    else:
+        location.append(f'{location} & Western')
+    return(location)
+
+
 
 with open('Meteorite_Landings.json', 'r') as f:
     ml_data = json.load(f)
@@ -23,4 +36,24 @@ with open('Meteorite_Landings.json', 'r') as f:
 print('The Number of Each Type of Meteorite in the Meteorite Landings Dataset is: ')
 pprint(meteor_classifier(ml_data['meteorite_landings'], 'recclass'))
 
+hemisphere_list = []
+for row in ml_data['meteorite_landings']:
+    hemisphere_list.append(check_hemisphere(float(row['reclat']), float(row['reclong'])))
+
+for j in range(len(hemisphere_list)):
+    del hemisphere_list[j][0]
+
+updated_hemisphere_list = []
+for k in range(len(hemisphere_list)):
+    updated_hemisphere_list.append(hemisphere_list[k][0])
+
+grouping_dict = {}
+for phrase in updated_hemisphere_list:
+    if phrase in grouping_dict:
+        grouping_dict[phrase] += 1
+    else:
+        grouping_dict[phrase] = 1
+
+print('The Part of the Hemisphere that the Meteorites Have Landed in are: ')
+print(grouping_dict)
 
