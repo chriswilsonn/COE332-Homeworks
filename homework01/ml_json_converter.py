79b8f042baee9c10dbce665c7db2e91ad31ca348
Ubuntu <ubuntu@coe332-vm-5.js2local>
2025-01-30 00:06:13 +0000

    wrote python code to convert the given meteorite landing json file into csv, xml, and yaml formats

---
+++ homework01/ml_json_converter.py
@@ -0,0 +1,33 @@
+import csv
+import json
+from dicttoxml import dicttoxml
+import xmltodict
+import yaml
+
+data = {}
+with open('Meteorite_Landings.json', 'r') as f:
+    data = json.load(f)
+
+with open('Meteorite_Landings.csv', 'w') as o:
+    csv_dict_writer = csv.DictWriter(o, data['meteorite_landings'][0].keys())
+    csv_dict_writer.writeheader()
+    csv_dict_writer.writerows(data['meteorite_landings'])
+
+xml_data = dicttoxml(data, custom_root = 'Meteorite_Landings', attr_type = False)
+dataxml = {}
+with open('Meteorite_Landings.xml', 'wb') as xml_file:
+    xml_file.write(xml_data)
+
+with open('Meteorite_Landings.xml', 'r') as p:
+    dataxml = xmltodict.parse(p.read())
+
+print(dataxml['Meteorite_Landings']['meteorite_landings']['item'][0]['name'])
+
+with open('Meteorite_Landings.yaml', 'w') as yaml_file:
+    yaml.dump(data, yaml_file)
+
+with open('Meteorite_Landings.yaml', 'r') as yaml_file:
+    yaml_data = yaml_file.read()
+
+print(yaml_data)
+
