9ffbbb50ee2b766b3fdf8b17a612635a6cba869a
Ubuntu <ubuntu@coe332-vm-5.js2local>
2025-01-30 00:34:20 +0000

    Added comments about the usage of AI

--- homework01/ml_json_converter.py
+++ homework01/ml_json_converter.py
@@ -13,6 +13,7 @@ with open('Meteorite_Landings.csv', 'w') as o:
     csv_dict_writer.writeheader()
     csv_dict_writer.writerows(data['meteorite_landings'])
 
+# Used AI to figure out how to convert from dict to xml
 xml_data = dicttoxml(data, custom_root = 'Meteorite_Landings', attr_type = False)
 dataxml = {}
 with open('Meteorite_Landings.xml', 'wb') as xml_file:
@@ -23,6 +24,7 @@ with open('Meteorite_Landings.xml', 'r') as p:
 
 print(dataxml['Meteorite_Landings']['meteorite_landings']['item'][0]['name'])
 
+# Used AI to figure out how to convert json to yaml file
 with open('Meteorite_Landings.yaml', 'w') as yaml_file:
     yaml.dump(data, yaml_file)
 
