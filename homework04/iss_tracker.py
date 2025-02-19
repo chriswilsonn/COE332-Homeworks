382dda4590a77151492089eaabacf72fffa27565
Ubuntu <ubuntu@coe332-vm-5.js2local>
2025-02-19 17:53:35 +0000

    wrote a function to check the daterange of the data in the dataset

--- homework04/iss_tracker.py
+++ homework04/iss_tracker.py
@@ -3,17 +3,32 @@ import requests
 from pprint import pprint
 import json
 import xmltodict
+from datetime import datetime
+
+
+def year_range(list_of_dicts: List[dict], key_str: str) -> tuple[str, str]:
+    date_str1 = list_of_dicts[0][key_str]
+    date_str2 = list_of_dicts[-1][key_str]
+    
+    datetime_obj1 = datetime.strptime(date_str1, "%Y-%jT%H:%M:%S.000Z")
+    datetime_obj2 = datetime.strptime(date_str2, "%Y-%jT%H:%M:%S.000Z")
+
+    formatted_date_1 = datetime_obj1.strftime("%B %d, %Y")
+    formatted_date_2 = datetime_obj2.strftime("%B %d, %Y")
+
+    return formatted_date_1, formatted_date_2
 
 def main():
     response = requests.get(url = 'https://nasa-public-data.s3.amazonaws.com/iss-coords/current/ISS_OEM/ISS.OEM_J2K_EPH.xml')
     data = xmltodict.parse(response.content.decode('utf-8')) # used AI to find right format
     json_data = json.dumps(data, indent = 2)
     dict_data = json.loads(json_data)
-    state_data = []
+    state_data = [] #list of dics
     for i in range(len(dict_data["ndm"]["oem"]["body"]["segment"]["data"]["stateVector"])):
         state_data.append(dict_data["ndm"]["oem"]["body"]["segment"]["data"]["stateVector"][i])
-
-    pprint(state_data[1]['EPOCH'])
+    
+    print(f"The data in this dataset is from {year_range(state_data,'EPOCH')[0]} to {year_range(state_data,'EPOCH')[1]}")
+    # print(year_range(state_data,'EPOCH'))
 
 if __name__ == '__main__':
     main()
