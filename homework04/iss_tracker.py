a2224aa6abbf829e20686052fec8c85e5bb5fe50
Ubuntu <ubuntu@coe332-vm-5.js2local>
2025-02-20 05:59:05 +0000

    wrote a function to find the average speed of the ISS as well as the current speed if the timesteps matched

--- homework04/iss_tracker.py
+++ homework04/iss_tracker.py
@@ -5,7 +5,7 @@ import json
 import xmltodict
 import time
 from datetime import datetime
-
+import math
 
 def year_range(list_of_dicts: List[dict], key_str: str) -> tuple[str, str]:
     '''
@@ -50,10 +50,40 @@ def matching_time(list_of_dicts: List[dict], key_str: str) -> dict:
         if current_time_s == iteration_time_s:
             return list_of_dicts[j]
         if j==(len(list_of_dicts) - 1):
-            print('No Match Found')
+            print('No Match Found for current time.')
         else:
             continue
 
+
+def speed(list_of_dicts: List[dict]) -> tuple[int, int]:
+    '''
+    Iterates through a list of dictionaries and finds the xdot, ydot, and zdot velocity components. It then squares each value and sums them together. Next that value is squre roo     ted to find the speed of each datapoint in the dataset. The speed values are then averaged to return the average speed of the ISS from the dataset. If the timestamp matches t      he timestamp when this program is run, the current speed is found using the same process as outlined above.
+    
+    Args:
+        list_of_dicts (list): a list of dictionaries that have the same set of keys.
+
+    Returns:
+        current_speed and average_speed: current speed returns the current speed of the ISS if the timestamps match whereas the average speed returns the average speed across the      dataset
+
+    '''
+    current_time_s = time.mktime(time.gmtime())
+    speed_vals = []
+    current_speed = 0
+    for k in range(len(list_of_dicts)):
+        iteration_time_s = time.mktime(time.strptime(list_of_dicts[k]['EPOCH'], '%Y-%jT%H:%M:%S.%fZ'))
+        x_dot = list_of_dicts[k]['X_DOT']['#text']
+        y_dot = list_of_dicts[k]['Y_DOT']['#text']
+        z_dot = list_of_dicts[k]['Z_DOT']['#text']
+        speed_vals.append(math.sqrt(float(x_dot)**2 + float(y_dot)**2 + float(z_dot)**2))
+        if current_time_s == iteration_time_s:
+            x_dot_1 = list_of_dicts[k]['X_DOT']['#text']
+            y_dot_1 = list_of_dicts[k]['Y_DOT']['#text']
+            z_dot_1 = list_of_dicts[k]['Z_DOT']['#text']
+            current_speed = math.sqrt(float(x_dot_1)**2 + float(y_dot_1)**2 + float(z_dot_1)**2)
+
+    average_speed = sum(speed_vals)/len(speed_vals)
+
+    return average_speed, current_speed
 
 def main():
     response = requests.get(url = 'https://nasa-public-data.s3.amazonaws.com/iss-coords/current/ISS_OEM/ISS.OEM_J2K_EPH.xml')
@@ -66,7 +96,7 @@ def main():
     
     print(f"The data in this dataset is from {year_range(state_data,'EPOCH')[0]} to {year_range(state_data,'EPOCH')[1]}")
     print(matching_time(state_data,'EPOCH'))
-    # print(year_range(state_data,'EPOCH'))
+    print(f"The average speed of the ISS based on this dataset is {speed(state_data)[0]} km/s and the current speed is {speed(state_data)[1]} km/s. If the current speed is 0, that means no matches were found for the current date." )
 
 if __name__ == '__main__':
     main()
