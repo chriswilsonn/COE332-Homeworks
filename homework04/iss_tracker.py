ad114c645ee9ab32f72fe1f635a69dcece78628d
Ubuntu <ubuntu@coe332-vm-5.js2local>
2025-02-20 19:34:39 +0000

    Edited year range function to have try except functionality for when date is entered in wrong format

--- homework04/iss_tracker.py
+++ homework04/iss_tracker.py
@@ -22,14 +22,16 @@ def year_range(list_of_dicts: List[dict], key_str: str) -> tuple[str, str]:
 
     date_str1 = list_of_dicts[0][key_str]
     date_str2 = list_of_dicts[-1][key_str]
-    
-    datetime_obj1 = datetime.strptime(date_str1, '%Y-%jT%H:%M:%S.%fZ')
-    datetime_obj2 = datetime.strptime(date_str2, '%Y-%jT%H:%M:%S.%fZ')
+    try:
+        datetime_obj1 = datetime.strptime(date_str1, '%Y-%jT%H:%M:%S.%fZ')
+        datetime_obj2 = datetime.strptime(date_str2, '%Y-%jT%H:%M:%S.%fZ')
 
-    formatted_date_1 = datetime_obj1.strftime("%B %d, %Y")
-    formatted_date_2 = datetime_obj2.strftime("%B %d, %Y")
+        formatted_date_1 = datetime_obj1.strftime("%B %d, %Y")
+        formatted_date_2 = datetime_obj2.strftime("%B %d, %Y")
 
-    return formatted_date_1, formatted_date_2
+        return formatted_date_1, formatted_date_2
+    except ValueError:
+        return 'Invalid Date Format'
 
 
 def matching_time(list_of_dicts: List[dict], key_str: str) -> dict:
@@ -97,7 +99,7 @@ def main():
     print(f"The data in this dataset is from {year_range(state_data,'EPOCH')[0]} to {year_range(state_data,'EPOCH')[1]}")
     print(matching_time(state_data,'EPOCH'))
     print(f"The average speed of the ISS based on this dataset is {speed(state_data)[0]} km/s and the current speed is {speed(state_data)[1]} km/s. If the current speed is 0, that means no matches were found for the current date." )
-
+    pprint(state_data)
 if __name__ == '__main__':
     main()
 
